45081406348956758ed71267f5f3ea572b584a31
Zengyu Qiu <39958763+Cuibaby@users.noreply.github.com>
2023-02-10 23:45:43 +0800

    Update vil100.py

--- configs/vil100/vil100.py
+++ configs/vil100/vil100.py
@@ -1,5 +1,5 @@
 net = dict(
-    type='RESANet'#
+    type='MFIALane'#
 )
 
 backbone = dict(
@@ -12,7 +12,7 @@ backbone = dict(
 #    in_channels=[64, 128, 256, -1],
 )
 
-resa = dict(
+mfia = dict(
     type='RESA',
     alpha=2.0,
     iter=2,
